e8ab168a05aa8d57038e01e3c626a9e6c6e83cd0
Alex Neep <neepalex@gmail.com>
2022-08-05 19:15:44 +0100

    Add button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+  children: string;
+  onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, onClick = () => {} }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="mx-auto flex items-center justify-center rounded bg-green-400 py-2 px-4 shadow-sm"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
